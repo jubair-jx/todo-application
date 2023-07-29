b6b7bde3f67301f832bb4da1eafd48deea15389f
jubair-jx <jubair.official97@gmail.com>
2023-07-29 12:54:39 +0600

    Action Reducers is working

--- src/Redux/Todos/todoReducer.js
+++ src/Redux/Todos/todoReducer.js
@@ -1,3 +1,62 @@
+import {
+  ADDEDTASK,
+  CLEARTASK,
+  COMPLETEDTASK,
+  SELECTEDCOLOR,
+  SINGLEDELETED,
+  TOGGELED,
+} from "./actionTypes";
 import { initlizeState } from "./initiliazeState";
 
-const todoReducer = (state = initlizeState, action) => {};
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+  return maxId + 1;
+};
+const todoReducer = (state = initlizeState, action) => {
+  switch (action.type) {
+    case ADDEDTASK:
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+        },
+      ];
+
+    case TOGGELED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !completed,
+        };
+      });
+    case SELECTEDCOLOR:
+      const { todoColor, todoId } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todoId;
+        }
+        return {
+          ...state,
+          color: todoColor,
+        };
+      });
+    case SINGLEDELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+    case COMPLETEDTASK:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+    case CLEARTASK:
+      return state.filter((todo) => !todo.completed);
+
+    default:
+  }
+};
+
+export default todoReducer;
